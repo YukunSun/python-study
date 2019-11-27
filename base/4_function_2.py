ef89109e713aec2026f1dee3673344592b19e052
sunyk <sunyukun@shopin.cn>
2019-11-27 23:21:54 +0800

    装饰器

--- base/4_function_2.py
+++ base/4_function_2.py
@@ -30,3 +30,70 @@ def func2():
 
 
 func2()
+
+
+# 装饰器2：如果函数想带上参数怎么办
+def tips(func):
+    def wrapper(a, b):
+        print("start")
+        func(a, b)
+        print("stop")
+
+    return wrapper
+
+
+@tips
+def add(a, b):
+    print(a + b)
+
+
+@tips
+def sub(a, b):
+    print(a - b)
+
+
+add(1, 2)
+sub(1, 2)
+
+
+# 输出：
+# start
+# 3
+# stop
+
+# start
+# -1
+# stop
+
+# 如何让装饰器传递参数：
+def new_tips(args):
+    def tips(func):
+        def wrapper(a, b):
+            print("start %s" % args)
+            func(a, b)
+            print("stop")
+
+        return wrapper
+
+    return tips
+
+
+@new_tips("haha 123")
+def add(a, b):
+    print(a + b)
+
+
+@new_tips("haha 456")
+def sub(a, b):
+    print(a - b)
+
+
+add(1, 3)
+sub(1, 3)
+# 输出：
+# start haha 123
+# 4
+# stop
+# start haha 456
+# -2
+# stop
