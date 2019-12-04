0b65dd4bda1c6af14cb67d7e7650f71ac2e9c3cf
sunyk <sunyukun@shopin.cn>
2019-12-04 23:36:51 +0800

    consumer,producer

---
+++ demo/curlDemo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def httpGet(url):
+    curl url
+
+
+httpGet("http://baidu.com")
---
+++ geek/hello_world.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import time
+
+print(time.time())
+
+if 10-9>0:
+    print('10>9')
+
+
+print(type(1))
+print(int('1'))
+
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+print(extra['city'])
