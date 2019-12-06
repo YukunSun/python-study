7e4b966f6da04daba7de228c56db882e059fb3cc
sunyk <sunyukun@shopin.cn>
2019-12-06 23:48:42 +0800

    requests

---
+++ base/12_requests.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import requests
+
+## get
+#url_1 = 'http://httpbin.org/get'
+#data = {'key': 'value', 'timeout': 5}
+#response = requests.get(url_1, data)
+#print(response.text)
+
+# post
+url_2 = 'http://httpbin.org/post'
+data_2 = {'key': 'value'}
+response2 = requests.post(url_2, data_2)
+print(response2.text)
