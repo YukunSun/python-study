b6da003efd62041eb3ecda1cf193e1d19b257b7c
sunyk <sunyukun@shopin.cn>
2019-12-07 13:22:40 +0800

    download imgs

--- base/13_spider.py
+++ base/13_spider.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests
 import re
+import os
+import shutil
 
 # 抓取图片
 
@@ -12,3 +14,12 @@ results = re.findall(pattern, content)
 
 for result in results:
     print(result[0], result[1])
+    # 下载图片
+    img_url = result[0]
+    response = requests.get(img_url, stream=True)
+    dir = os.path.abspath('.')
+    img_path = os.path.join(dir, result[1] + '.jpg')
+    if response.status_code == 200:
+        with open(img_path, 'wb') as f:
+            response.raw.deconde_content = True
+            shutil.copyfileobj(response.raw, f)
